3f09c411cd74972efae55b8f31a59eae06fc803e
binchoo <079111w@gmail.com>
2020-07-19 12:52:09 +0900

    Move sortCriterion so that it becomes constructor parameter.

--- src/main/kotlin/rsspoller/sort/SortStrategy.kt
+++ src/main/kotlin/rsspoller/sort/SortStrategy.kt
@@ -3,9 +3,7 @@ package rsspoller.sort
 import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
 
-open class SortStrategy<CriterionType: Comparable<CriterionType>>(sortCriterion: SortCriterion<CriterionType>, val isAscending: Boolean) {
-    private var sortCriterion: SortCriterion<CriterionType> = sortCriterion
-
+open class SortStrategy<CriterionType: Comparable<CriterionType>>(private var sortCriterion: SortCriterion<CriterionType>, val isAscending: Boolean) {
     fun sort(elements: Elements) {
         when (isAscending) {
             true -> elements.sortBy { element ->
@@ -23,50 +21,50 @@ open class SortStrategy<CriterionType: Comparable<CriterionType>>(sortCriterion:
 
     class AttrValue(private val attrKey: String, isAscending: Boolean = true)
         : SortStrategy<String>(object: SortCriterion<String> {
-            override fun criterionValueOf(element: Element): String {
-                return element.attr(attrKey)
-            }
-        }, isAscending)
+        override fun criterionValueOf(element: Element): String {
+            return element.attr(attrKey)
+        }
+    }, isAscending)
 
     class ChildrenCount(isAscending: Boolean = true)
         : SortStrategy<Int>(object: SortCriterion<Int> {
-            override fun criterionValueOf(element: Element): Int {
-                return element.children().size
-            }
-        }, isAscending)
+        override fun criterionValueOf(element: Element): Int {
+            return element.children().size
+        }
+    }, isAscending)
 
     class ClassName(isAscending: Boolean = true)
         : SortStrategy<String>(object: SortCriterion<String> {
-            override fun criterionValueOf(element: Element): String {
-                return element.className()
-            }
-        }, isAscending)
+        override fun criterionValueOf(element: Element): String {
+            return element.className()
+        }
+    }, isAscending)
 
     class TagName(isAscending: Boolean = true)
         : SortStrategy<String>(object: SortCriterion<String> {
-            override fun criterionValueOf(element: Element): String {
-                return element.tagName()
-            }
-        }, isAscending)
+        override fun criterionValueOf(element: Element): String {
+            return element.tagName()
+        }
+    }, isAscending)
 
     class Text(isAscending: Boolean = true)
         : SortStrategy<String>(object: SortCriterion<String> {
-            override fun criterionValueOf(element: Element): String {
-                return element.text()
-            }
-        }, isAscending)
+        override fun criterionValueOf(element: Element): String {
+            return element.text()
+        }
+    }, isAscending)
 
     class TextLength(isAscending: Boolean = true)
         : SortStrategy<Int>(object: SortCriterion<Int> {
-            override fun criterionValueOf(element: Element): Int {
-                return element.text().length
-            }
-        }, isAscending)
+        override fun criterionValueOf(element: Element): Int {
+            return element.text().length
+        }
+    }, isAscending)
 
     class HtmlLength(isAscending: Boolean = true)
         : SortStrategy<Int>(object: SortCriterion<Int> {
-            override fun criterionValueOf(element: Element): Int {
-                return element.html().length
-            }
-        }, isAscending)
+        override fun criterionValueOf(element: Element): Int {
+            return element.html().length
+        }
+    }, isAscending)
 }
